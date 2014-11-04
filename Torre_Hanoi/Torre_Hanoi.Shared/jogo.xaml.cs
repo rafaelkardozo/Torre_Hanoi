d77af3fb3e7e7fab21176ecb1dadad91416b1744
rafaelkardozo <rafaelkard@gmail.com>
2014-11-04 16:39:25 -0200

    Metodo de validação Desempilhar
    Metodo responsavel por controlar se será possivel retirar um elemento da
    pilha ou não.

--- Torre_Hanoi/Torre_Hanoi.Shared/jogo.xaml.cs
+++ Torre_Hanoi/Torre_Hanoi.Shared/jogo.xaml.cs
@@ -49,7 +49,7 @@ namespace Torre_Hanoi
             IniciaJogo();
         }
 
-        // BOTAO DE EMPILHAR TORRE1
+        // BOTAO EMPILHAR TORRE1
         private void bt_empilhar_torre1_Click(object sender, RoutedEventArgs e)
         {
 
@@ -62,36 +62,53 @@ namespace Torre_Hanoi
 
         }//FIM DO BOTAO EMPILHAR TORRE1
 
+        //BOTAO DESEMPILHAR TORRE1
+        private void bt_desempilhar_torre1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         
 
 
         
-
+        //METODO PARA INICIAR JOGO
         private void IniciaJogo(){
             
+            //PREENCHENDO A PILHA TORRE1
             torre1.Push(4); torre1.Push(3); torre1.Push(2); torre1.Push(1);
+            
+            //TORNANDO VISIVEIS AS ARGOLAS NA TORRE1
             img_argola1.Visibility = Visibility.Visible;
             img_argola2.Visibility = Visibility.Visible;
             img_argola3.Visibility = Visibility.Visible;
             img_argola4.Visibility = Visibility.Visible;
-
+            
+            //TORNANDO VISIVEIS OS BOTOES DAS TORRES
             bt_empilhar_torre1.Visibility = Visibility.Visible; bt_desempilhar_torre1.Visibility = Visibility.Visible;
             bt_empilhar_torre2.Visibility = Visibility.Visible; bt_desempilhar_torre2.Visibility = Visibility.Visible;
             bt_empilhar_torre3.Visibility = Visibility.Visible; bt_desempilhar_torre3.Visibility = Visibility.Visible;
 
-
-
-
-            
-
         }
-
+        //FIM DO METODO PARA INCIAR JOGO
+        
+        //METODO DE VALIDACAO DE EMPILHAMENTO DOS DISCOS
         private bool ValidaEmpilhamento(int tamanhoTorre, int topo, int aux ){
 
             if ((tamanhoTorre < 4)||(topo<aux) ) return true;
             else return false;
 
         }
+        //FIM DO METODO DE VALIDACAO DE EMPILHAMENTO
+
+        //METODO DE VALIDACAO DESEMPILHAR
+        private bool ValidaDesempilhamento(int tamanho)
+        {
+            if (tamanho > 0) return true;
+            else return false;
+
+        }
+
 
 
         
